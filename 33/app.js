01868f7e8f1b06797aa984120b3738bcf8a47b5c
JYeonDev <whddus4837@gmail.com>
2021-08-03 21:56:05 +0900

    에러발생 미해결

--- 33/app.js
+++ 33/app.js
@@ -1,7 +1,20 @@
-const body = document.getElementsByTagName('body');
+const body = document.body;
 const changeButton = document.querySelectorAll('.circle');
+const colors = ['gray', '#fff', 'blue', 'yellow'];
 
-
-const bgChange = () => {
-  
-}
+changeButton.forEach(button => {
+  button.addEventListener('click', () => {
+    const contain = (circle) => {
+      button.classList.contains(circle)
+    }
+    if(contain(circle1)) {
+      body.style.backgroundColor = colors[0];      
+    } else if(contain(circle2)) {
+      body.style.backgroundColor = colors[1];
+    } else if(contain(circle3)) {
+      body.style.backgroundColor = colors[2];
+    } else if(contain(circle4)) {
+      body.style.backgroundColor = colors[3];
+    }
+  })
+});
