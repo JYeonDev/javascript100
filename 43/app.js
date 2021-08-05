4834c0b74b5c649584fddad35c1b0d669ef49411
JYeonDev <whddus4837@gmail.com>
2021-08-05 13:24:45 +0900

    이미지 슬라이더 완료

--- 43/app.js
+++ 43/app.js
@@ -1,12 +1,22 @@
 const btn = document.querySelectorAll('.btn');
 const img = document.querySelector('.main_image');
 
+
+let count = 1;
 btn.forEach(button => {
   button.addEventListener('click', (e) => {
-    let count = 1;
     if(e.target.id === 'arrow-left') {
       img.setAttribute('src', `./images/image${count}.png`);
+      count--;     
+      if(count === 0) {
+        count = 4;
+      }
+    } else if (e.target.id === 'arrow-right') {
+      img.setAttribute('src', `./images/image${count}.png`);
       count++;
+      if (count === 5) {
+        count = 1;
+      }
     }
   })
 })
