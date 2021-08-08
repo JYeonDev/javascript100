0e723227e07f9fe66546d12731c02f2dc0e60db0
JYeonDev <whddus4837@gmail.com>
2021-08-08 12:08:43 +0900

    window 화면 랜덤출력 남음

--- 75/app.js
+++ 75/app.js
@@ -1,11 +1,15 @@
 const disappear = document.getElementById('beginStarting');
 const appear = document.querySelector('.toggle_two')
 const btn = document.querySelector('.btn');
+const circle = document.querySelector('.circle');
+const colors = ['red', 'black', 'green', 'yellow', 'purple', 'orange', 'pink']
 
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', () => {
   disappear.classList.add('toggle_first')
-  appear.classList.remove('toggle_two')
-
+  setInterval(() => {
+    appear.classList.remove('toggle_two')
+  circle.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+  }, Math.floor(Math.random() * 2000));
 })
 
-console.log(window.innerHeight, window.innerWidth)
+console.log(window.innerHeight + 'px', window.innerWidth + 'px')
